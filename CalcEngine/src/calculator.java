d4ea4b532108058598fa584be574755b74abc05b
George <George@Charlie>
2014-12-10 16:16:15 -0500

    set4

--- CalcEngine/src/calculator.java
+++ CalcEngine/src/calculator.java
@@ -1,3 +1,26 @@
 public class calculator {
-
+	private double InvestmentAmount;
+	private int Years;
+	private double AnnualInterestRate;
+	
+	public calculator(double Inverst, int year, double Rate){
+		InvestmentAmount = Inverst;
+		Years = year;
+		AnnualInterestRate = Rate;
+	}
+	
+	double MonthlyRate = AnnualInterestRate / 12;
+	int time = Years * 12;
+	public double Future(){
+		double future = InvestmentAmount;
+		for(int i = 0; i < time; i++){
+			future = future * (1 + MonthlyRate);
+		}
+		return future;
+	}
+	
+	public static void main(String[] arg){
+		calculator c = new calculator(10000, 3, 3.25);
+		System.out.println(c.Future());
+	}
 }
